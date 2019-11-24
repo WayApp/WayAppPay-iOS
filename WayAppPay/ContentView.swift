c9b799559f6c07822e55aa52bccd799bacb4360f
Oscar Anzola <oanzola@wayapp.com>
2019-11-24 19:53:33 +0100

    Initial Commit

---
+++ WayAppPay/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  WayAppPay
+//
+//  Created by Oscar Anzola on 11/24/19.
+//  Copyright © 2019 WayApp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
